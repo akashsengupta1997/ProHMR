de9dcc38b50b6d5b0c10fdb734596f4e588d1738
Akash Sengupta <akash.sengupta1997@gmail.com>
2022-11-02 14:46:41 +0000

    Update renderer.py

--- prohmr/utils/renderer.py
+++ prohmr/utils/renderer.py
@@ -60,6 +60,8 @@ class Renderer:
                                                    point_size=1.0)
 
         self.camera_center = [self.img_res // 2, self.img_res // 2]
+        print('HERE2', self.img_res, self.camera_center)
+
         self.faces = faces
 
     def __call__(self,
